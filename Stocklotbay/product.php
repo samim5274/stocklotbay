fe6b54caaa07955a132b6babe00cdef34f91f4d3
Samim Hossen <85032804+samim5274@users.noreply.github.com>
2024-03-18 18:56:32 +0600

    updated

---
+++ Stocklotbay/product.php
@@ -0,0 +1,218 @@
+
+<!--=========================== product section start ===========================-->
+
+
+
+<section id="men-section">
+    <div class="container">
+        <div class="row">
+            <h3 class="heading">Men</h3>
+            <div class="col-lg-3 col-md-3 col-sm-6">
+                <a href="#">
+                    <div class="card">
+                        <img class="card-img-top" src="img/product/stocklot Bay (4).jpg" alt="Card image cap">
+                        <div class="card-body">
+                        <h4 class="card-title text-danger">Price: $5.40/-</4>
+                        <h6 class="card-title">Boys Solid T-shirt Boys Solid T-shirt Boys Solid T-shirt </h6>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-6">
+               <a href="#">
+                    <div class="card">
+                        <img class="card-img-top" src="img/product/stocklot Bay (3).jpg" alt="Card image cap">
+                        <div class="card-body">
+                        <h4 class="card-title text-danger">Price: $5.40/-</4>
+                        <h6 class="card-title">Full Sleeve Boys Solid T-shirt Boys Solid T-shirt Boys Solid T-shirt </h6>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-6">
+                <a href="#">
+                    <div class="card">
+                        <img class="card-img-top" src="img/product/stocklot Bay (2).jpg" alt="Card image cap">
+                        <div class="card-body">
+                        <h4 class="card-title text-danger">Price: $5.40/-</4>
+                        <h6 class="card-title">Boys Solid T-shirt Boys Solid T-shirt Boys Solid T-shirt </h6>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-6">
+                <a href="#">
+                    <div class="card">
+                        <img class="card-img-top" src="img/product/stocklot Bay (1).jpg" alt="Card image cap">
+                        <div class="card-body">
+                        <h4 class="card-title text-danger">Price: $5.40/-</4>
+                        <h6 class="card-title">Boys Solid T-shirt Boys Solid T-shirt Boys Solid T-shirt </h6>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section id="women-section">
+    <div class="container">
+        <div class="row">
+            <h3 class="heading">Women</h3>
+            <div class="col-lg-3 col-md-3 col-sm-6">
+                <a href="#">
+                    <div class="card">
+                        <img class="card-img-top" src="img/product/stocklot Bay (4).jpg" alt="Card image cap">
+                        <div class="card-body">
+                        <h4 class="card-title text-danger">Price: $5.40/-</4>
+                        <h6 class="card-title">Boys Solid T-shirt Boys Solid T-shirt Boys Solid T-shirt </h6>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-6">
+               <a href="#">
+                    <div class="card">
+                        <img class="card-img-top" src="img/product/stocklot Bay (3).jpg" alt="Card image cap">
+                        <div class="card-body">
+                        <h4 class="card-title text-danger">Price: $5.40/-</4>
+                        <h6 class="card-title">Full Sleeve Boys Solid T-shirt Boys Solid T-shirt Boys Solid T-shirt </h6>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-6">
+                <a href="#">
+                    <div class="card">
+                        <img class="card-img-top" src="img/product/stocklot Bay (2).jpg" alt="Card image cap">
+                        <div class="card-body">
+                        <h4 class="card-title text-danger">Price: $5.40/-</4>
+                        <h6 class="card-title">Boys Solid T-shirt Boys Solid T-shirt Boys Solid T-shirt </h6>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-6">
+                <a href="#">
+                    <div class="card">
+                        <img class="card-img-top" src="img/product/stocklot Bay (1).jpg" alt="Card image cap">
+                        <div class="card-body">
+                        <h4 class="card-title text-danger">Price: $5.40/-</4>
+                        <h6 class="card-title">Boys Solid T-shirt Boys Solid T-shirt Boys Solid T-shirt </h6>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section id="boy-section">
+    <div class="container">
+        <div class="row">
+            <h3 class="heading">Boy</h3>
+            <div class="col-lg-3 col-md-3 col-sm-6">
+                <a href="#">
+                    <div class="card">
+                        <img class="card-img-top" src="img/product/stocklot Bay (4).jpg" alt="Card image cap">
+                        <div class="card-body">
+                        <h4 class="card-title text-danger">Price: $5.40/-</4>
+                        <h6 class="card-title">Boys Solid T-shirt Boys Solid T-shirt Boys Solid T-shirt </h6>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-6">
+               <a href="#">
+                    <div class="card">
+                        <img class="card-img-top" src="img/product/stocklot Bay (3).jpg" alt="Card image cap">
+                        <div class="card-body">
+                        <h4 class="card-title text-danger">Price: $5.40/-</4>
+                        <h6 class="card-title">Full Sleeve Boys Solid T-shirt Boys Solid T-shirt Boys Solid T-shirt </h6>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-6">
+                <a href="#">
+                    <div class="card">
+                        <img class="card-img-top" src="img/product/stocklot Bay (2).jpg" alt="Card image cap">
+                        <div class="card-body">
+                        <h4 class="card-title text-danger">Price: $5.40/-</4>
+                        <h6 class="card-title">Boys Solid T-shirt Boys Solid T-shirt Boys Solid T-shirt </h6>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-6">
+                <a href="#">
+                    <div class="card">
+                        <img class="card-img-top" src="img/product/stocklot Bay (1).jpg" alt="Card image cap">
+                        <div class="card-body">
+                        <h4 class="card-title text-danger">Price: $5.40/-</4>
+                        <h6 class="card-title">Boys Solid T-shirt Boys Solid T-shirt Boys Solid T-shirt </h6>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section id="girl-section">
+    <div class="container">
+        <div class="row">
+            <h3 class="heading">Girl</h3>
+            <div class="col-lg-3 col-md-3 col-sm-6">
+                <a href="#">
+                    <div class="card">
+                        <img class="card-img-top" src="img/product/stocklot Bay (4).jpg" alt="Card image cap">
+                        <div class="card-body">
+                        <h4 class="card-title text-danger">Price: $5.40/-</4>
+                        <h6 class="card-title">Boys Solid T-shirt Boys Solid T-shirt Boys Solid T-shirt </h6>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-6">
+               <a href="#">
+                    <div class="card">
+                        <img class="card-img-top" src="img/product/stocklot Bay (3).jpg" alt="Card image cap">
+                        <div class="card-body">
+                        <h4 class="card-title text-danger">Price: $5.40/-</4>
+                        <h6 class="card-title">Full Sleeve Boys Solid T-shirt Boys Solid T-shirt Boys Solid T-shirt </h6>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-6">
+                <a href="#">
+                    <div class="card">
+                        <img class="card-img-top" src="img/product/stocklot Bay (2).jpg" alt="Card image cap">
+                        <div class="card-body">
+                        <h4 class="card-title text-danger">Price: $5.40/-</4>
+                        <h6 class="card-title">Boys Solid T-shirt Boys Solid T-shirt Boys Solid T-shirt </h6>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-6">
+                <a href="#">
+                    <div class="card">
+                        <img class="card-img-top" src="img/product/stocklot Bay (1).jpg" alt="Card image cap">
+                        <div class="card-body">
+                        <h4 class="card-title text-danger">Price: $5.40/-</4>
+                        <h6 class="card-title">Boys Solid T-shirt Boys Solid T-shirt Boys Solid T-shirt </h6>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<!--=========================== product section start ===========================-->
---
+++ Stocklotbay/slide.php
@@ -0,0 +1,49 @@
+<!--=========================== slide section start ===========================-->
+
+<div id="carouselExample" class="carousel slide">
+  <div class="carousel-inner">
+    <div class="carousel-item carousel-img-1 active">
+        <div class="container">
+            <div class="carousel-caption mb-5 pb-5  text-dark">
+                <img src="img/1.jpg" alt="" class="pdr_img">
+                <h2 class="display-4">Monitize your surplus garments</h2>
+                <p class="lead">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis, nihil ipsam eaque blanditiis dolorem ut!</p>
+                <a href="#" class="btn btn-warning">Shop Now</a>
+            </div>
+        </div>
+    </div>
+    <div class="carousel-item carousel-img-2">
+        <div class="container">
+            <div class="carousel-caption mb-5 pb-5 text-dark">
+            <img src="img/2.jpg" alt="" class="pdr_img">
+                <h2 class="display-4">Release surplouse garments</h2>
+                <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio obcaecati libero est aperiam. Quam, iusto.</p>
+                <a href="#" class="btn btn-warning">Shop Now</a>
+            </div>
+        </div>
+    </div>
+    <div class="carousel-item carousel-img-3">
+        <div class="container">
+            <div class="carousel-caption mb-5 pb-5 text-dark">
+            <img src="img/3.jpg" alt="" class="pdr_img">
+                <h2 class="display-4">Buy Stocklot garments</h2>
+                <p class="lead">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel possimus, magni dolorem provident ex cupiditate!</p>
+                <a href="#" class="btn btn-warning">Shop Now</a>
+            </div>
+        </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+<!--=========================== slide section end ===========================-->
+
+
+
