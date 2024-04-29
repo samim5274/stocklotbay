0828533028092d9de439d2373cf00eb611ed5e77
Samim Hossen <85032804+samim5274@users.noreply.github.com>
2024-04-29 20:25:51 +0600

    Update

--- Stocklotbay/myAccount.php
+++ Stocklotbay/myAccount.php
@@ -68,19 +68,19 @@ include 'menuBar.php';
 
         <div class="col-lg-6">
           <h2 class="fw-bold mb-5 text-start">Register</h2>
-          <form>
+          <form action="backend" method="post">
             <!-- 2 column grid layout with text inputs for the first and last names -->
             <div class="row">
               <div class="col-md-6 mb-4">
                 <div data-mdb-input-init class="form-outline text-start">
                     <label class="form-label" for="form3Example1">First name</label>
-                    <input type="text" id="form3Example1" class="form-control" />                  
+                    <input name="txtfirstname" type="text" id="form3Example1" class="form-control" />                  
                 </div>
               </div>
               <div class="col-md-6 mb-4">
                 <div data-mdb-input-init class="form-outline text-start">
                     <label class="form-label" for="form3Example2">Last name</label>
-                    <input type="text" id="form3Example2" class="form-control" />                  
+                    <input name="txtlastname" type="text" id="form3Example2" class="form-control" />                  
                 </div>
               </div>
             </div>
@@ -88,44 +88,50 @@ include 'menuBar.php';
             <!-- Phone input -->
             <div data-mdb-input-init class="form-outline mb-4 text-start">
                 <label class="form-label" for="form3Example5">Phone</label>
-                <input type="email" id="form3Example5" class="form-control" />                
+                <input name="txtphone" type="number" id="form3Example5" class="form-control" />                
             </div>
 
             <!-- Username input -->
             <div data-mdb-input-init class="form-outline mb-4 text-start">
                 <label class="form-label" for="form3Example6">Username (username cann't be changed)</label>
-                <input type="email" id="form3Example6" class="form-control" />
+                <input name="txtusername" type="text" id="form3Example6" class="form-control" />
             </div>
 
             <!-- Email input -->
             <div data-mdb-input-init class="form-outline mb-4 text-start">
                 <label class="form-label" for="form3Example113">Email address</label>
-                <input type="email" id="form3Example113" class="form-control" />                
+                <input name="txtemail" type="email" id="form3Example113" class="form-control" />                
             </div>
 
             <!-- Password input -->
             <div data-mdb-input-init class="form-outline mb-4 text-start">
                 <label class="form-label" for="form3Example4">Password</label>
-                <input type="password" id="pass1" class="form-control" />                
+                <input name="txtpassword" type="password" id="pass1" class="form-control" />                
             </div>
 
             <!-- CPassword input -->
             <div data-mdb-input-init class="form-outline mb-4 text-start">
                 <label class="form-label" for="form3Example7">Confirm Password</label>
-                <input type="password" id="Cpass2" class="form-control" />
+                <input name="txtcpassword" type="password" id="Cpass2" class="form-control" />
                 <span class="missmatch" id = "message1" style="color:green">  <span class="missmatch2" id = "message2" style="color:red">     
+            </div>
+
+            <!-- Image input -->
+            <div data-mdb-input-init class="form-outline mb-4 text-start">
+                <label name="flimage" class="form-label" for="image">Upload your image (Recomended 300 x 300)</label>
+                <input type="file" id="image" class="form-control" />  
             </div>
 
             <!-- Checkbox -->
             <div class="form-check d-flex text-start mb-4">
-              <input class="form-check-input me-2" type="checkbox" value="" id="form2Example1" />
+              <input name="chkcheckbox" class="form-check-input me-2" type="checkbox" value="1" id="form2Example1" />
               <label class="form-check-label" for="form2Example1">
-                Subscribe to our newsletter
+                I accepet all <a href="https://www.facebook.com/SAMIM5274.NET?" target="_blank">trams & condition.</a> 
               </label>
             </div>
 
             <!-- Submit button -->
-            <button onkeyup="passValid()" type="submit" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-block mb-4">
+            <button type="submit" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-block mb-4">
                 Register
             </button>
           </form>
